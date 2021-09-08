1c168d7bbb1b9f9e6670e70eef88207fcf2adc2f
joshualiangxy <joshualiang.xy@gmail.com>
2021-09-08 21:37:36 +0800

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import config from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'https://notes-app-frontend-qjkfjkawhq-as.a.run.app',
+    },
+  });
   const { port } = config();
 
   await app.listen(port);
